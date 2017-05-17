91231c81522414eed9ca36153900118b3b5e977c
Thomas Lisankie <link285@gmail.com>
2017-05-17 15:28:14 -0400

    Starting to learn Rust

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+
+    println!("Welcome to the guessing game!");
+    println!("Guess the number between 1 and 100!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 100);
+
+    let mut guess = String::new(); // "::" in this case is a static method.
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less    => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal   => println!("You win!"),
+    }
+
+    println!("The secret number was: {}", secret_number);
+
+}
